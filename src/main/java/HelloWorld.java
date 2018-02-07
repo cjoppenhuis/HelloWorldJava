f6d2d499c488bc1f59491577ba3274978092d061
Jelle <jelle.oppenhuis@hva.nl>
2018-02-07 22:44:40 +0100

    java stuff

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+    }
+
+    public int countTwoNumbers(int first, int second) {
+        return first+second;
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,20 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testCountFunction() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+
+        int firstNumber = 1;
+        int secondNumber = 2;
+
+        int answerOfFunction = helloWorld.countTwoNumbers(firstNumber, secondNumber);
+
+        Assert.assertThat(answerOfFunction, CoreMatchers.is(3));
+        Assert.assertThat(answerOfFunction, CoreMatchers.is(2));
+    }
+
+}
